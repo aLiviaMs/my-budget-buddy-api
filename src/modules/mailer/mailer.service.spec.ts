96f3cbb090580e5b346b8028a05f2f9a7ba6e5a8
Livia <liviawrk@gmail.com>
2024-07-29 22:12:16 -0300

    test: update mailer service specs

--- src/modules/mailer/mailer.service.spec.ts
+++ src/modules/mailer/mailer.service.spec.ts
@@ -5,16 +5,28 @@ import { BadRequestException } from '@nestjs/common';
 // Services
 import { MailerService } from './mailer.service';
 import { PrismaService } from '../../shared/database/prisma.service';
+import { MailerService as MailerMain } from '@nestjs-modules/mailer';
 
 // Models
 import { CreateUserDto } from '../users/models/dto';
+
+const mockMailerMain = {
+  sendMail: jest.fn()
+};
 
 describe('MailerService', () => {
   let service: MailerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailerService, PrismaService]
+      providers: [
+        MailerService,
+        PrismaService,
+        {
+          provide: MailerMain,
+          useValue: mockMailerMain
+        }
+      ]
     }).compile();
 
     service = module.get<MailerService>(MailerService);
@@ -25,28 +37,36 @@ describe('MailerService', () => {
   });
 
   describe('sendWelcomeEmail', () => {
-    it('should create a new user record and return that', async () => {
+    it('should send a welcome email', async () => {
       const user: CreateUserDto = {
         email: 'user@example.com',
         name: 'user',
         password: 'password'
       };
 
-      expect(await service.sendWelcomeEmail(user)).toEqual(expect.any(Object));
+      await service.sendWelcomeEmail(user);
+
+      expect(mockMailerMain.sendMail).toHaveBeenCalledWith({
+        to: user.email,
+        from: `"Oi do seu maior parceiro :)" <${process.env.SMTP_USER}>`,
+        subject: 'Bem vindo ao Budget Buddy!',
+        text: `Olá ${user.name},\n\nBem-vindo ao Budget Buddy! Estamos empolgados para ajudar você a organizar suas despesas financeiras.`,
+        html: expect.any(String)
+      });
     });
 
-    it('should create a new user record and return that', async () => {
+    it('should throw an error if sendMail fails', async () => {
       const user: CreateUserDto = {
         email: 'user@example.com',
         name: 'user',
         password: 'password'
       };
 
-      jest.spyOn(service, 'sendWelcomeEmail').mockImplementation(() => {
-        throw BadRequestException;
+      mockMailerMain.sendMail.mockImplementationOnce(() => {
+        throw new BadRequestException();
       });
 
-      expect(await service.sendWelcomeEmail(user)).rejects.toThrow(BadRequestException);
+      await expect(service.sendWelcomeEmail(user)).rejects.toThrow(BadRequestException);
     });
   });
 });
